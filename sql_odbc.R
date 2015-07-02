994565a61859bae2708b75a5c25307c23d1fbb21
unknown <bmaher@l106518.na.leapfrog.com>
2015-07-01 17:19:20 -0700

    Connect to SP_REPORTING and Pull TOYS SEGMENT3 POS

---
+++ sql_odbc.R
@@ -0,0 +1,12 @@
+
+require(RODBC)
+odbcConnect()
+conn <- odbcDriverConnect('driver={SQL Server};server=NAEMSQL02\\SPREPORTING;database=SP_REPORTING;trusted_connection=true')
+sql <- "SELECT SEGMENT3,POS_YEAR,POS_MTH_NBR,SUM(POS_UNITS) as units
+  FROM R_POS_VIEW_MONTHLY
+  WHERE SEGMENT1 = 'LEARNING TOYS'
+  GROUP BY POS_YEAR, POS_MTH_NBR,SEGMENT1,SEGMENT2,SEGMENT3
+  ORDER BY POS_YEAR, POS_MTH_NBR"
+sql <- strwrap(sql, width=10000, simplify=TRUE)
+TOYS <- sqlQuery(conn, sql)
+TOYS.ALL <- split(TOYS,TOYS$SEGMENT3)
