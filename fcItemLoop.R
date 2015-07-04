7229791131fd009ded5ebe75bf82a2d775894a1c
Ben Maher <ben@benjaminmaher.com>
2015-07-04 08:01:52 -0700

    reIndent

--- fcItemLoop.R
+++ fcItemLoop.R
@@ -17,33 +17,33 @@ library(forecast)
 ##Take DataFrame from LAPPLY and generate parent TS object
 
 fcMulti <- function(x,last_actual,lookback_years) {
-    fcTStoYR(fcItemToTS(x),last_actual,lookback_years)  
+     fcTStoYR(fcItemToTS(x),last_actual,lookback_years)  
 }
 
 fcItemToTS <- function(x) {
-    preTS <- x[x$units!=0,]
-    This.start <- unlist(head(preTS[2:3],1))
-    This.end <- unlist(tail(preTS[2:3],1))
-    This.TS <- ts(preTS$units,start=This.start,frequency=12)
-    This.TS
+     preTS <- x[x$units!=0,]
+     This.start <- unlist(head(preTS[2:3],1))
+     This.end <- unlist(tail(preTS[2:3],1))
+     This.TS <- ts(preTS$units,start=This.start,frequency=12)
+     This.TS
 }
 
 fcTStoYR <- function(x,last_actual,lookback_years) {
-  f <- 12
-    
-    #Store length of passed timeseries  
-    start.win <- trunc(last_actual)-lookback_years
-    end.win <- last_actual
-    
-    #Store desired range of resulting timeseries
-    xend <- end(x)[1]+end(x)[2]/f
-    xstart <- start(x)[1]+start(x)[2]/f
-    
-    ##make sure we have enough data
-    if(end.win>xend){ return("data ends too soon")}
-    if(start.win<xstart){ return("data starts too late")}
-  
-  ##use window to subset
-  window(x,start=start.win,end=end.win)
+     f <- 12
+     
+     #Store length of passed timeseries  
+     start.win <- trunc(last_actual)-lookback_years
+     end.win <- last_actual
+     
+     #Store desired range of resulting timeseries
+     xend <- end(x)[1]+end(x)[2]/f
+     xstart <- start(x)[1]+start(x)[2]/f
+     
+     ##make sure we have enough data
+     if(end.win>xend){ return("data ends too soon")}
+     if(start.win<xstart){ return("data starts too late")}
+     
+     ##use window to subset
+     window(x,start=start.win,end=end.win)
 }
 
